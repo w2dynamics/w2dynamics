2a9e6c29de9406c4783e2913e03a4ee97302f24e
Alexander Kowalski <akowalski@physik.uni-wuerzburg.de>
2022-07-05 22:05:38 +0200

    Raise code version and date

--- w2dyn/auxiliaries/__init__.py
+++ w2dyn/auxiliaries/__init__.py
@@ -19,7 +19,7 @@ BANNER = r"""
                                        Version %s, %s
 """
 
-CODE_VERSION = 1, 1, "2"
+CODE_VERSION = 1, 1, "3"
 CODE_VERSION_STRING = ".".join(map(str,CODE_VERSION))
-CODE_DATE = "April 2022"
+CODE_DATE = "July 2022"
 OUTPUT_VERSION = 2, 2
